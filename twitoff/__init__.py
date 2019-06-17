4b83b2f2d9baabd180bf933823e7c7141ff8219a
dustiny5 <dustiny5@users.noreply.github.com>
2019-06-17 13:53:49 -0700

    Make modules to create app

--- twitoff/__init__.py
+++ twitoff/__init__.py
@@ -1,5 +1,18 @@
-""" Entry point for TwitOff. """
+"""
+Entry point for TwitOff.
+"""
 
 from .app import create_app
 
 APP = create_app()
+
+# 1. Go to TwitOff directiory
+#   - In gitbash, 
+#       - type - pipenv install Flask
+#       - type - pipenv shell - activates virtual environment
+#       - type - FLASK_APP=twitoff flask run
+#       - Or type - FLASK_APP=twitoff:APP flask run
+#       - type - pipenv install flask-sqlalchemy
+#           - to run sql
+#       - type - FLASK_APP=twitoff:APP flask shell
+#           - To run flask stuff

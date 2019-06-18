2d4c55bc3e1ea3d431a88f26fd00176c7fbe57c0
dustiny5 <dustiny5@users.noreply.github.com>
2019-06-18 14:54:25 -0700

    'Keys'

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,19 +1,30 @@
 """
-Main application and routing logic for TwitOff
+Main application and routing/configuration logic for TwitOff
 """
 
-from flask import Flask
-from .models import DB
+from flask import Flask, render_template, request
+from .models import DB, User
+from decouple import config # Reads our .env file
 
 def create_app():
     """ Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Warning goes away
+    app.config['ENV'] = config('ENV') # Shows detailed error # Don't deploy
     DB.init_app(app)
 
-
+    # Root route
     @app.route('/')
     def root():
-        return 'Hello TwitOff!'
+        users = User.query.all()
+        # Look inside template directory
+        return render_template('base.html', title='Home', users=users)
     
+    # Convenient reset
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset!', users=[])
     return app

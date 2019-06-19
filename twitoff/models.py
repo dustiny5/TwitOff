29299ba40c1f8627b08554c7bdcada1af71103a9
dustiny5 <dustiny5@users.noreply.github.com>
2019-06-18 20:10:47 -0700

    Update notes under user=DB.relationship...

--- twitoff/models.py
+++ twitoff/models.py
@@ -19,6 +19,9 @@ class Tweet(DB.Model):
     text = DB.Column(DB.Unicode(500))
     embedding = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
+    # Creates an empty list user.tweets.
+    # Append tweets to user.tweets
+    # Bypasses join and can get tweets easier
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):

e0d6e0eb262766397a55070065de04cfe30c97f5
dustiny5 <dustiny5@users.noreply.github.com>
2019-06-20 09:18:16 -0700

    Update predict on app and prediction html

--- twitoff/app.py
+++ twitoff/app.py
@@ -6,7 +6,7 @@ from decouple import config # Reads our .env file
 from flask import Flask, render_template, request
 from .models import DB, User
 from .twitter import add_or_update_user
-
+from .predict import predict_user
 
 def create_app():
     """ Create and configure an instance of the Flask application."""
@@ -30,7 +30,7 @@ def create_app():
         DB.drop_all()
         DB.create_all()
         return render_template('base.html', title='DB Reset!', users=[])
-
+    # Apply to user.html
     @app.route('/user', methods=['POST'])
     @app.route('/user/<name>', methods=['GET'])
     def user(name=None, message=''):
@@ -46,7 +46,22 @@ def create_app():
             message = 'Error adding {}: {}'.format(name, e)
             tweets = []
         return render_template('user.html', title=name, tweets=tweets, message=message)
+    # Apply to predict.html
+    @app.route('compare', methods=['POST'])
+        user1, user2 = sorted([request.values['user1'], 
+                               request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+            else:
+                prediction = predict_user(user1, user2,
+                                           request.values['tweet_text'])
+                message = '"{}" is more likely to be said by {} than {}'.format(
+                    request.values['tweet_text'], user1 if prediction else user2,
+                    user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
 
+
+    
     '''
     # Class
     @app.route('/user', methods=['POST'])
